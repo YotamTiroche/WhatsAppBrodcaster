f10f7129f0d79b6ba8fa061dd1e9c5fb7e36c81c
ShakeNBake7000 <yotam011@gmail.com>
2024-03-12 17:41:18 +0200

    added basic interface and readme file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # WhatsAppBrodcaster
 Automate bulk message sending on WhatsApp using Selenium!
+
+# How to Setup
+1. Clone the project and make sure you have a [Python Runtime](https://code.visualstudio.com/docs/languages/python)
+2. Download ChromeDriver. It is needed in order for the script to be able to open and use Google Chrome.
+Find the compatible version [here](https://googlechromelabs.github.io/chrome-for-testing/)
+3. Once you picked and downloaded your compatible version, place chromedriver.exe into your root directory, right next to the script you cloned.
+4. Last and most important - your contact list. 
+In order to use the script the contacts have to be saved on your phone by the same names as in the contact list. This is because using a normal whatsapp brodcast list won't send the message to the recipient if you are not saved on their phone. The script bypasses that by sending each contact on the list a private message.
+
+To save the contact list on your phone in one click I reccomend google contacts. Use the 'Import contacts' feature and pick the csv file, then connect your phone to the app and boom, all contacts saved on your phone!
+
+# How to use
+The script takes two inputs:
+- a .txt file containing the message you want to send to your clients
+- a .csv file containing the list of contact names for which you want to brodcast the message. the format needed for the csv file is a column called 'name' and values for each contact.
+
+Once all that is done, all thats left is running the script, filling the console promts
+and enjoy the hours of manual work saved!
--- brodcaster_script.py
+++ brodcaster_script.py
@@ -5,13 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 
 import time, csv
 
 # Setup Variables
-message_filename = 'message.txt' # Enter desired message filename here
-contacts_filename = 'purimatlanta-Participants_googleformat.csv' # Enter contacts CSV filename here
+message_filename = input('Please enter message filename (txt file only)') 
+contacts_filename = input('Please enter contacts list filename (csv file only)')
 
 # Pre-written function used to paste content using JS (required when using chrome)
 def paste_content(driver, el, content): 
@@ -60,19 +60,22 @@ for contact in contacts:
     try:
         contact_box = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span[title="' + contact + '"]'))).click()
         time.sleep(3)  # Add a delay to avoid rapid sending
+        # Select the message box and send the message
+        message_box = driver.find_element(By.CSS_SELECTOR, "#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._1VZX7 > div._3Uu1_ > div > div.to2l77zo.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt > p")
+        
+        #paste_content(driver, message_box, message)
+        ActionChains(driver).send_keys_to_element(message_box, message).perform()
+        time.sleep(3)
+        message_box.send_keys(Keys.ENTER)
+        sucsessful_count = sucsessful_count + 1
+        time.sleep(3)
     except:
         print("Contact not found: " + contact)
         search_box.send_keys(Keys.CONTROL + "a")
         search_box.send_keys(Keys.DELETE)
         failed_count = failed_count + 1
         continue
-    # Select the message box and send the message
-    message_box = driver.find_element(By.CSS_SELECTOR, "#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._1VZX7 > div._3Uu1_ > div > div.to2l77zo.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt > p")
-    paste_content(driver, message_box, message)
-    time.sleep(3)
-    message_box.send_keys(Keys.ENTER)
-    sucsessful_count = sucsessful_count + 1
-    time.sleep(3)
+
 
 # Close the browser
 time.sleep(10)
